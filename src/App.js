bde3dd8dbc9bb9b5f9b0379db89de418b0d2f520
Mehdi Najafi <mehdinajafi20.mn@gmail.com>
2020-07-23 17:04:52 +0430

    Create routing project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Blog from './components/Blog';
+import Menue from './components/Menue';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+    document.onreadystatechange = function() { 
+        if (document.readyState !== "complete") { 
+            document.querySelector("body").style.visibility = "hidden"; 
+            document.querySelector("#loader").style.visibility = "visible"; 
+        } else { 
+            document.querySelector("#loader").style.display = "none"; 
+            document.querySelector("body").style.visibility = "visible"; 
+        } 
+    };
+    return (
+        <Router>
+            <div id="loader" className="center"></div> 
+            <Header/>
+            <Switch>
+                <Route exact path = "/">
+                    <Home/>
+                </Route>
+                <Route path="/menue">
+                    <Menue/>
+                </Route>
+                <Route path="/contact-us">
+                    <Contact/>
+                </Route>
+                <Route path="/blog">
+                    <Blog/>
+                </Route>
+            </Switch>
+            <Footer/>
+        </Router>
+    )
 }
 
 export default App;
---
+++ src/components/Menue.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menue = () => {
+    return (
+        <div>
+            Menue
+        </div>
+    )
+}
+
+export default Menue;
