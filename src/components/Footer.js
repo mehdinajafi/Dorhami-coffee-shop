9e3e40adafb6978487021df2df04acf7717a35f6
Mehdi Najafi <mehdinajafi20.mn@gmail.com>
2020-07-23 20:14:10 +0430

    Create footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
+import '../styles/Footer.css'
 
-const Footer = () => {
+function Footer () {
     return (
-        <div>
-            Footer
-        </div>
+        <section id="footer">
+            کلیه حقوق وب سایت متعلق به بستنی دورهمی می باشد. طراحی و توسعه توسط<a href="mehdi.najafi">مهدی نجفی</a>
+        </section>
     )
 }
 
